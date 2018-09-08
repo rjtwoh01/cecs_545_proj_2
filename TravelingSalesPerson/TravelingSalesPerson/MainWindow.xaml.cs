963d6a2b600075cd6356f9a151437742280b5e2d
rjtwoh01 <rjtwoh01@louisville.edu>
2018-09-08 13:11:14 -0400

    Initial Version
    Taken from project 1

---
+++ TravelingSalesPerson/TravelingSalesPerson/MainWindow.xaml.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace TravelingSalesPerson
+{
+    public partial class MainWindow : Window
+    {
+        private string tspFileName;
+        private List<Point> tspPoints;
+        private Canvas canvas;
+        private Viewbox viewbox;
+        private TSP tsp;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            tspFileName = "";
+            hideRunTime();
+            hideSolveButton();
+            tspPoints = new List<Point>();
+        }
+
+        #region Data Points
+
+        private void populatePoints()
+        {
+            List<Point> newPoints = new List<Point>();
+            using (StreamReader stream = new StreamReader(tspFileName))
+            {
+                string fileLine;
+                bool coordinates = false;
+
+                while ((fileLine = stream.ReadLine()) != null)
+                {
+                    string[] parts = fileLine.Split(' ');
+                    if (coordinates)
+                    {
+                        if (parts.Length >= 3)
+                        {
+                            Point newPoint = new Point(Convert.ToDouble(parts[1]), Convert.ToDouble(parts[2]));
+                            tspPoints.Add(newPoint);
+                            newPoints.Add(newPoint);
+                            Debug.WriteLine(newPoint);
+                        }
+                    }
+                    else if (fileLine == "NODE_COORD_SECTION")
+                    {
+                        coordinates = true;
+                    }
+                }
+            }
+            plotPoints(newPoints);
+        }
+
+        private void plotPoints(List<Point> points)
+        {
+            int city = 1; //we start at the first city
+            tsp = new TSP(points);
+
+            viewbox = new Viewbox();
+            viewbox.HorizontalAlignment = HorizontalAlignment.Stretch;
+            viewbox.VerticalAlignment = VerticalAlignment.Stretch;
+
+            canvas = new Canvas();
+
+            foreach (Point point in points)
+            {
+                Debug.WriteLine("City: " + city);
+                Ellipse ellipse = new Ellipse();
+                ellipse.Width = 4;
+                ellipse.Height = 4;
+                ellipse.Fill = Brushes.Red;
+                ellipse.Stroke = Brushes.Black;
+
+                ellipse.ToolTip = city + ": (" + point.X + "," + point.Y + ")";
+
+                // Position point on canvas
+                Canvas.SetLeft(ellipse, point.X + tsp.canvasOffset.X);
+                Canvas.SetTop(ellipse, point.Y + tsp.canvasOffset.Y);
+
+                canvas.Children.Add(ellipse);
+
+                city++;
+            }
+
+            canvas.Height = tsp.maxPoint.Y - tsp.minPoint.Y + 80;
+            canvas.Width = tsp.maxPoint.X - tsp.minPoint.X + 80;
+            Debug.WriteLine(canvas.Height);
+            Debug.WriteLine(canvas.Width);
+
+            viewbox.Child = canvas;
+            mainGrid.Children.Add(viewbox);
+
+            Debug.WriteLine(mainGrid.Children[0]);
+
+            //this.UpdateLayout();
+            Debug.WriteLine("Finished populating points");
+            
+        }
+
+        public void drawLines(List<Point> fastestRoute)
+        {
+            Polygon pathLine = new Polygon();
+            pathLine.Stroke = Brushes.Black;
+
+            Ellipse ellipse = canvas.Children[0] as Ellipse;
+            if (ellipse != null)
+            {
+                ellipse.Fill = Brushes.Green;
+            }
+
+            for (int i = 0; i < fastestRoute.Count(); i++)
+            {
+                if ((i + 1) != fastestRoute.Count())
+                {
+                    //(fastestRoute[i], fastestRoute[i + 1]);
+                    Point point = fastestRoute[i];
+                    point.X += tsp.canvasOffset.X + 2;
+                    point.Y += tsp.canvasOffset.Y + 2;
+
+                    pathLine.Points.Add(point);
+                }
+            }
+            canvas.Children.Insert(0, pathLine);
+        }
+
+        #endregion
+
+        #region UI Elements
+
+        public void emptyCanvas()
+        {
+            if (canvas != null)
+                this.canvas.Children.Clear();
+            this.tspPoints.Clear();
+        }
+
+        public void hideSolveButton()
+        {
+            this.btnSolve.Visibility = Visibility.Hidden;
+        }
+
+        public void showSolveButton()
+        {
+            this.btnSolve.Visibility = Visibility.Visible;
+        }
+
+        public void displayRunTime()
+        {
+            this.lblRunTime.Visibility = Visibility.Visible;
+            this.UpdateLayout();
+        }
+
+        public void hideRunTime()
+        {
+            this.lblRunTime.Visibility = Visibility.Hidden;
+            this.UpdateLayout();
+        }
+
+        #endregion
+
+        #region UI Events
+
+        //Slightly modified for my purposes from: https://stackoverflow.com/questions/10315188/open-file-dialog-and-select-a-file-using-wpf-controls-and-c-sharp
+        private void btnFileUpload_Click(object sender, RoutedEventArgs e)
+        {
+            emptyCanvas();
+            OpenFileDialog dlg = new OpenFileDialog();
+
+            dlg.DefaultExt = ".tsp";
+            dlg.Filter = "TSP Files|*.tsp";
+
+            Nullable<bool> result = dlg.ShowDialog();
+            if (result == true)
+            {
+                string fileName = dlg.FileName;
+                Debug.WriteLine(fileName);
+                this.tspFileName = fileName;
+                populatePoints();
+            }
+
+            hideRunTime();
+            showSolveButton();
+        }
+
+
+        private void btnSolve_Click(object sender, RoutedEventArgs e)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            List<Point> tempResult = tsp.BruteForce();
+            sw.Stop();
+
+            TimeSpan elapsedTime = sw.Elapsed;
+            string shortestDistance = String.Format("{0:0.00}", tsp.shortestDistance);
+            this.lblRunTime.Content = "Distance: "+ shortestDistance + "\nRun Time: " + elapsedTime.ToString();
+
+            displayRunTime();
+            drawLines(tempResult);
+        }
+
+        #endregion
+    }
+}
---
+++ TravelingSalesPerson/TravelingSalesPerson/TSP.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Diagnostics;
+using System.Collections;
+
+namespace TravelingSalesPerson
+{
+    class TSP
+    {
+        public Point canvasOffset; //Not sure if we need this yet
+        public Point maxPoint;
+        public Point minPoint;
+        public List<Point> points;
+        public List<Point> tempFinalList;
+
+        public double shortestDistance;
+
+        public TSP(List<Point> points)
+        {
+            //Values for UI purposes, creating offsets for the grid
+            this.points = new List<Point>();
+            this.minPoint = points.First();
+            this.maxPoint = points.First();
+            this.tempFinalList = new List<Point>();
+
+            foreach(Point point in points)
+            {
+                this.points.Add(point);
+            }
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                Point point = new Point(points[i].X, points[i].Y);
+
+                if (point.X < this.minPoint.X) { this.minPoint.X = point.X; }
+                else if (point.X > this.maxPoint.X) { this.maxPoint.X = point.X; }
+                if (point.Y < this.minPoint.Y) { this.minPoint.Y = point.Y; }                
+                else if (point.Y > this.maxPoint.Y) { this.maxPoint.Y = point.Y; }
+            }
+
+            this.canvasOffset = new Point(10, 10);
+
+            if (this.minPoint.X > 0) { this.canvasOffset.X -= this.minPoint.X; }
+            else { this.canvasOffset.X += this.minPoint.X; }
+            if (this.minPoint.Y > 0) { this.canvasOffset.X -= this.minPoint.X; }
+            else { this.canvasOffset.X += this.minPoint.X; }
+
+            this.shortestDistance = 0;
+        }
+
+        public double distance(Point pointOne, Point pointTwo)
+        {
+            return Math.Sqrt(Math.Pow((pointTwo.X - pointOne.X), 2) + Math.Pow((pointTwo.Y - pointOne.Y), 2));
+        }
+
+        public List<Point> BruteForce()
+        {
+            //This final list will represent the correct order - or path - to take
+            List<Point> finalList = new List<Point>();
+            var tempList = new List<Point>();
+            var newList = new List<Point>();
+            double localDistance = 0;
+            shortestDistance = 0;
+            int totalPermutations = 0;
+            int initialCount = 0;
+
+            foreach (Point point in this.points)
+            {
+                tempList.Add(point);
+            }
+
+            initialCount = tempList.Count();
+
+            Point firstElement = tempList.First();
+            List<Point> rest = tempList;
+            rest.RemoveAt(0);
+
+            //Iterate through each permutaion
+            foreach(var perm in Permutate(rest, rest.Count()))
+            {
+                double shortestSoFar = shortestDistance;
+                localDistance = 0;
+                newList.Clear();
+                newList.Add(firstElement); //we start with the same city every time
+                //Iterate through each element in this particular permutation
+                foreach (var i in perm)
+                {
+                    //We need to read the element as a string because it is no longer recognized as a point
+                    //Once we have the strong, it can be converted back to a point and added to the new list
+                    string[] parts = i.ToString().Split(',');
+                    Point tempPoint = new Point(Convert.ToDouble(parts[0]), Convert.ToDouble(parts[1]));
+                    newList.Add(tempPoint);
+                }
+                newList.Add(firstElement); //we end with the same city every time
+                //Calculate the distance
+                for (int i = 0; i < newList.Count(); i++)
+                {
+                    if ((i + 1) != newList.Count())
+                        localDistance += distance(newList[i], newList[i + 1]);
+                }
+                //Check if this should be a canidate for the final list
+                if (shortestDistance > localDistance || shortestDistance == 0)
+                {
+                    shortestDistance = localDistance;
+                    finalList.Clear();
+                    finalList = newList.ToList(); //Save computation time of foreach
+                }
+            }
+
+            int city = 1;
+            Debug.WriteLine("\nFinal list: ");
+            foreach (Point point in finalList)
+            {
+                Debug.WriteLine(city + ": " + point);
+                city++;
+            }
+            Debug.WriteLine("\nTotal Run Distance: " + shortestDistance + "\nTotal Permutations: " + totalPermutations);
+
+            return finalList;
+        }
+
+        #region Permutation
+
+        //The following two functions are implemented from: https://www.codeproject.com/Articles/43767/A-C-List-Permutation-Iterator
+
+        public static void RotateRight(IList sequence, int count)
+        {
+            object tmp = sequence[count - 1];
+            sequence.RemoveAt(count - 1);
+            sequence.Insert(0, tmp);
+        }
+
+        public static IEnumerable<IList> Permutate(IList sequence, int count)
+        {
+            if (count == 1) yield return sequence;
+            else
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    foreach (var perm in Permutate(sequence, count - 1))
+                        yield return perm;
+                    RotateRight(sequence, count);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
